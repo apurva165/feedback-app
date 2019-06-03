30e9f00ffaafc418cd474b7a09af00aad40dab65
Apurva165 <Apurva165@gmail.com>
2019-06-02 23:33:50 -0500

    working

--- server/index.js
+++ server/index.js
@@ -5,10 +5,25 @@ const app = express();
 const passport = require("passport");
 const googleStrategy = require("passport-google-oauth20").Strategy;
 
-//passport.use(new googleStrategy());
-// app.get("/", (req, res) => {
-//   res.send({ hi: "there" });
-// });
+passport.use(
+  new googleStrategy(
+    {
+      clientID: keys.googleClientId,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "auth/google/callback"
+    },
+    accesstoken => {
+      console.log(accesstoken);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
